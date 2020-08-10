5f846cf60af1d212ed681b7af0bfcdf14694d286
Natalie Wong <wongn341@gmail.com>
2020-08-09 23:02:32 -0400

    Added more properties to my models

--- Velocity.Data/VelocityContext.cs
+++ Velocity.Data/VelocityContext.cs
@@ -23,6 +23,16 @@ namespace Velocity.Data
         [ForeignKey("Container")]
         public Guid ContainerId { get; set; }
 
+        [Timestamp]
+        [DisplayName("Drop Off Date")]
+        [Description("The drop off time of the container.")]
+        public DateTime FromDate { get; set; }
+
+        [Timestamp]
+        [DisplayName("Pick Up Date")]
+        [Description("The pick up time of the container.")]
+        public DateTime ToDate { get; set; }
+
         [DisplayName("Pick Up")]
         [Description("The pick up location of the container.")]
         public string FromAddress { get; set; }
@@ -30,11 +40,22 @@ namespace Velocity.Data
         [DisplayName("Drop Off")]
         [Description("The drop off location of the container.")]
         public string ToAddress { get; set; }
+
+        [DisplayName("In Transit")]
+        [Description("Whether the container is in transit.")]
+        public bool IsPending { get; set; }
     }
 
     public class Invoice
     {
         public Guid Id { get; set; }
+
+        [Timestamp]
+        [DisplayName("Invoice Date")]
+        [Description("The date of the invoice.")]
+        public DateTime InvoiceDate { get; set; }
+
+        // Fees, Amount, Company/Client
     }
 
     public class Container
@@ -44,12 +65,16 @@ namespace Velocity.Data
         // Unique
         public string ContainerNumber { get; set; }
 
+        // Will EF interpret it as a FK relationship?
+        public Client Client { get; set; } 
+
     }
 
     public class Client
     {
         public Guid Id { get; set; }
 
+        // Todo: Make some nullable 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
