96b4e52008cf4d497e8ecdb15857db37bc8bd5c1
Natalie Wong <wongn341@gmail.com>
2020-08-10 23:43:51 -0400

    Add ICrudRepository and a first draft ClientRepository that implements it.

---
+++ Velocity.Core/Interfaces/ICrudRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Velocity.Core.Interfaces
+{
+    public interface ICrudRepository<TModel>
+        where TModel : class, new()
+    {
+        public void Create(TModel model);
+        public Task CreateAsync(TModel model);
+
+        public void Delete(TModel model);
+        public Task DeleteAsync(TModel model);
+
+        public void Update(TModel model);
+        public Task UpdateAsync(TModel model);
+
+        public TModel Get(Guid id);
+        public Task<TModel> GetAsync(Guid id);
+        public IQueryable<TModel> Get(Expression<Func<TModel, bool>> predicate);
+        public Task<IQueryable<TModel>> GetAsync(Expression<Func<TModel, bool>> predicate);
+
+    }
+}
---
+++ Velocity.Core/Repository/ClientRepository.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Velocity.Core.Interfaces;
+using Velocity.Data;
+
+namespace Velocity.Core.Repository
+{
+    public class ClientRepository : ICrudRepository<Client>
+    {
+        private readonly VelocityContext context;
+
+        public ClientRepository(VelocityContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Client model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            context.Clients.Add(model);
+            context.SaveChanges();
+        }
+
+        public async Task CreateAsync(Client model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            await context.Clients.AddAsync(model);
+            await context.SaveChangesAsync();
+        }
+
+        public void Delete(Client model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            Client client = Get(model.Id);
+
+            if (client is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            context.Clients.Remove(client);
+        }
+
+        public Task DeleteAsync(Client model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client Get(Guid id)
+        {
+            return context.Clients.Find(id) ?? throw new ArgumentException(nameof(id));
+        }
+
+        public IQueryable<Client> Get(string companyName)
+        {
+            return context.Clients.Where(c => c.CompanyName.Equals(companyName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IQueryable<Client> Get(Expression<Func<Client, bool>> predicate)
+        {
+            return context.Clients.Where(predicate);
+        }
+
+        public async Task<Client> GetAsync(Guid id)
+        {
+            return await context.Clients.FindAsync(id);
+        }
+
+        // find a better way. currently not async
+        public async Task<IQueryable<Client>> GetAsync(Expression<Func<Client, bool>> predicate)
+        {
+            return context.Clients.Where(predicate);
+        }
+
+        public void Update(Client model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            Client client = Get(model.Id);
+
+            if (client is null)
+            {
+                throw new ArgumentException("This client cannot be updated as it doesn't exist.");
+            }
+
+            context.Clients.Update(client);
+            context.SaveChanges();
+        }
+
+        public async Task UpdateAsync(Client model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            Client client = Get(model.Id);
+
+            if (client is null)
+            {
+                throw new ArgumentException("This client cannot be updated as it doesn't exist.");
+            }
+
+            context.Clients.Update(client);
+            await context.SaveChangesAsync();
+        }
+    }
+}
