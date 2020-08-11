98562c8db108033266b6e8694aa01d5afe54bb04
Natalie Wong <wongn341@gmail.com>
2020-08-10 23:43:15 -0400

    Add TransitState enum

---
+++ Velocity.Data/Enums/TransitState.cs
@@ -0,0 +1,23 @@
+﻿namespace Velocity.Data.Enums
+{
+    /// <summary>
+    /// Represents the state in transit for a given <see cref="Container"/>.
+    /// </summary>
+    public enum TransitState
+    {
+        /// <summary>
+        /// The container is ready to be driven by the driver.
+        /// </summary>
+        Ready,
+
+        /// <summary>
+        /// Currently driving the truck to transport the container.
+        /// </summary>
+        InTransit,
+
+        /// <summary>
+        /// The driver has successfully delivered the container to client location.
+        /// </summary>
+        Delivered
+    }
+}
