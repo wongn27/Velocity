53d3eec274e94051cb8077af023cb0eaea667750
Natalie Wong <wongn341@gmail.com>
2020-08-29 22:05:34 -0400

    + InvoiceDetailRepository

---
+++ Velocity.Core/Repository/InvoiceDetailRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Velocity.Data;
+using Velocity.Data.Models;
+
+namespace Velocity.Core.Repository
+{
+    public class InvoiceDetailRepository : CrudRepositoryBase<InvoiceDetail>
+    {
+        private InvoiceRepository invoiceRepository;
+
+        public InvoiceDetailRepository(VelocityContext context) : base(context)
+        {
+            invoiceRepository = new InvoiceRepository(context);
+        }
+
+        public IQueryable<InvoiceDetail> GetInvoiceDetails(Guid invoiceId)
+        {
+            var invoice = invoiceRepository.Get(invoiceId);
+
+            if (invoice is null)
+            {
+                throw new ArgumentException("The specified invoice does not exist.");
+            }
+
+            return Get(detail => detail.InvoiceId == invoiceId);
+        }
+    }
+}
